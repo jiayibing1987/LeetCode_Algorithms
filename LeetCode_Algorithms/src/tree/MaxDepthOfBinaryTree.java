3a8ece48def03d34cf2af32f1d7211ea4c0d463e
yibing jia <jiayibing1987@gmail.com>
2019-07-22 01:36:25 +1000

    a clear solution of #104

--- LeetCode_Algorithms/src/tree/MaxDepthOfBinaryTree.java
+++ LeetCode_Algorithms/src/tree/MaxDepthOfBinaryTree.java
@@ -2,10 +2,16 @@ package tree;
 
 import util.TreeNode;
 
+/**
+ * 104. Maximum Depth of Binary Tree
+ * Given a binary tree, find its maximum depth.
+ * @author Ryan
+ *
+ */
 public class MaxDepthOfBinaryTree {
 
 	int max = 0;
-    public int maxDepth(TreeNode root) {
+    public int maxDepth1(TreeNode root) {
         maxHelper(root, 0);
         return max;
     	
@@ -21,13 +27,13 @@ public class MaxDepthOfBinaryTree {
     	maxHelper(root.right, count);
     }
     
-    /** if the node does not exist, simply return 0. Otherwise, return the 1+the longer distance of its subtree.
+    // if the node does not exist, simply return 0. Otherwise, return the 1+the longer distance of its subtree.
     public int maxDepth(TreeNode root) {
         if(root==null){
             return 0;
         }
         return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
-    } */
+    } 
     
     public static void main(String[] args){
     	TreeNode node1 = new TreeNode(1);
