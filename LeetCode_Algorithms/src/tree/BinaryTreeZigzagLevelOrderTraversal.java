acd3967b5d7e16ad8729c65494c496e485311e0f
yibing jia <jiayibing1987@gmail.com>
2019-07-22 20:47:26 +1000

    #103 commit

---
+++ LeetCode_Algorithms/src/tree/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,42 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import util.TreeNode;
+
+/**
+ * 103. Binary Tree Zigzag Level Order Traversal
+ * @author Ryan
+ *
+ */
+public class BinaryTreeZigzagLevelOrderTraversal {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+        if(root == null) return results;
+        dfs(results, 0, root);
+        return results;
+    }
+    
+    public void dfs(List<List<Integer>> results, int depth, TreeNode node){
+    	if(node == null) return;
+    	if(depth == results.size())//get one level deeper, need to create a new list in results
+    		results.add(new LinkedList<Integer>());
+    	if(depth%2 == 0) 
+    		results.get(depth).add(node.val);
+    	else {
+    		LinkedList<Integer> list = (LinkedList<Integer>) results.get(depth);
+    		list.offerFirst(node.val);
+    	}
+    	dfs(results, depth+1, node.left);
+    	dfs(results, depth+1, node.right);
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
