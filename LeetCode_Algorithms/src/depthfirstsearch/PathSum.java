bc5b2188108bd7daa8b9d022345aa943cf9cd1ea
yibing jia <jiayibing1987@gmail.com>
2019-07-25 23:45:15 +1000

    #122 with cleaner code

--- LeetCode_Algorithms/src/depthfirstsearch/PathSum.java
+++ LeetCode_Algorithms/src/depthfirstsearch/PathSum.java
@@ -3,45 +3,60 @@ package depthfirstsearch;
 import util.TreeNode;
 
 /**
- * #112
- * Given a binary tree and a sum, 
- * determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ * #112 Given a binary tree and a sum, determine if the tree has a root-to-leaf
+ * path such that adding up all the values along the path equals the given sum.
  */
 public class PathSum {
 
-	public boolean hasPathSum(TreeNode root, int sum) {	
-		if(root == null)
+	public boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null)
+			return false;
+		return dfs(root, sum, 0);
+	}
+
+	public boolean dfs(TreeNode node, int targetSum, int currentSum) {
+		if (node.left == null && node.right == null) {
+			if (currentSum + node.val == targetSum)
+				return true;
+			else
+				return false;
+		}
+		return (node.left == null ? false : dfs(node.left, targetSum, currentSum + node.val))
+				|| (node.right == null ? false : dfs(node.right, targetSum, currentSum + node.val));
+	}
+
+	public boolean hasPathSum1(TreeNode root, int sum) {
+		if (root == null)
 			return false;
 		return helper(root, sum, 0);
 	}
 
-	private boolean helper(TreeNode node, int sum, int currentSum){
-		if(node.left == null && node.right == null){
-			if(currentSum + node.val == sum)
+	private boolean helper(TreeNode node, int sum, int currentSum) {
+		if (node.left == null && node.right == null) {
+			if (currentSum + node.val == sum)
 				return true;
 			else
 				return false;
 		}
 		boolean foundLeft = false;
 		boolean foundRight = false;
-		if(node.left != null)
+		if (node.left != null)
 			foundLeft = helper(node.left, sum, currentSum + node.val);
-		if(node.right != null)
+		if (node.right != null)
 			foundRight = helper(node.right, sum, currentSum + node.val);
 		return foundLeft || foundRight;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		PathSum p = new PathSum();
-		
+
 		TreeNode node1 = new TreeNode(5);
 		TreeNode node2 = new TreeNode(4);
 		TreeNode node3 = new TreeNode(8);
-		
+
 		node1.left = node2;
 		node1.right = node3;
-	
+
 		System.out.println(p.hasPathSum(node1, 13));
 	}
 
