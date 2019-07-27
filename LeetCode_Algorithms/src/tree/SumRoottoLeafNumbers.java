3dd85f185e65117be14e2b5f65ae0d2b830b8760
yibing jia <jiayibing1987@gmail.com>
2019-07-27 17:57:07 +1000

    second solution for #129, better code

--- LeetCode_Algorithms/src/tree/SumRoottoLeafNumbers.java
+++ LeetCode_Algorithms/src/tree/SumRoottoLeafNumbers.java
@@ -10,8 +10,27 @@ import util.TreeNode;
  */
 public class SumRoottoLeafNumbers {
 
-	int sum;
+	//solution2, not using a global variable, clean code
     public int sumNumbers(TreeNode root) {
+    	return helper(root, 0);
+    }
+    
+	private int helper(TreeNode node, int num){
+		if(node == null)
+			return 0;
+		if(node.left == null && node.right==null)
+			return num*10 + node.val;
+		return helper(node.left, num*10 + node.val)
+				+ helper(node.right, num*10 + node.val);
+	}
+	
+	
+	
+	
+	
+    //solution 1 , append strings
+	int sum;
+    public int sumNumbers1(TreeNode root) {
     	sum = 0;
     	if(root == null) return 0;
         dfs(root, "");
@@ -20,9 +39,8 @@ public class SumRoottoLeafNumbers {
     
     //pre-order 
     private void dfs (TreeNode node, String s){
-    	if(node.left == null && node.right == null){
+    	if(node.left == null && node.right == null)
     		sum += Integer.valueOf(s+String.valueOf(node.val));
-    	}
     	if(node.left != null) dfs(node.left, s+String.valueOf(node.val));
     	if(node.right != null) dfs(node.right, s+String.valueOf(node.val));
     }
