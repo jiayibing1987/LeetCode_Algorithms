f2460a4e2ec10d20a5ba27b2c4da15a0b0a2ab7b
yibing jia <jiayibing1987@gmail.com>
2019-09-11 17:15:45 +1000

    #152

--- LeetCode_Algorithms/src/dynamicprogramming/MaxProductSubarray.java
+++ LeetCode_Algorithms/src/dynamicprogramming/MaxProductSubarray.java
@@ -1,31 +1,30 @@
 package dynamicprogramming;
 
 /**
- * 
+ * 152. Maximum Product Subarray
  *
  */
 public class MaxProductSubarray {
 
-    public int maxProduct(int[] nums) {
-    	int res = nums[0];
-    	//max product value until index i
-    	int[] p = new int[nums.length];
-    	//min product value until index i
-    	int[] n = new int[nums.length];
-    	p[0] = nums[0];
-    	n[0] = nums[0];
-    	
-    	for(int i=1; i<nums.length; i++){
-    		//pi can be max value of pi-1*numi(both positive) or ni-1*numi(both negative)
-    		//if pi is 0 then just take numi
-    		p[i] = Math.max(Math.max(p[i-1]*nums[i], n[i-1]*nums[i]), nums[i]);
-    		n[i] = Math.min(Math.min(p[i-1]*nums[i], n[i-1]*nums[i]), nums[i]);
-    		res = Math.max(res, p[i]);
-    	}
-    	
-    	return res;
-    }
-	
+	public int maxProduct(int[] nums) {
+		int res = nums[0];
+		// max product value until index i
+		int[] max = new int[nums.length];
+		// min product value until index i
+		int[] min = new int[nums.length];
+		max[0] = nums[0];
+		min[0] = nums[0];
+
+		for (int i = 1; i < nums.length; i++) {
+			max[i] = Math.max(Math.max(max[i - 1] * nums[i], min[i - 1] * nums[i]), nums[i]);
+			min[i] = Math.min(Math.min(min[i - 1] * nums[i], max[i - 1] * nums[i]), nums[i]);
+		}
+		for (int i = 1; i < nums.length; i++) {
+			res = Math.max(res, max[i]);
+		}
+		return res;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
