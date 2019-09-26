24e0e1bf54b7dda4ffa295aa77fb8f05a699b2a3
yibing jia <jiayibing1987@gmail.com>
2019-09-27 02:19:33 +1000

    #34 improve solution

--- LeetCode_Algorithms/src/binarysearch/FirstandLastPosition.java
+++ LeetCode_Algorithms/src/binarysearch/FirstandLastPosition.java
@@ -15,6 +15,42 @@ import java.util.Arrays;
 public class FirstandLastPosition {
 
 	public int[] searchRange(int[] nums, int target) {
+		int leftPos = binarySearch(nums, target, 0);
+		if(leftPos == -1) return new int[] {-1, -1};
+		int rightPos = binarySearch(nums, target, 1);
+		return new int[]{leftPos, rightPos};
+	}
+	
+	private int binarySearch(int[]nums, int target, int mod){
+		if(nums.length == 0) return -1;
+		int low = 0;
+		int high = nums.length-1;
+		while(low < high){
+			int mid = (low+high)/2;
+			if(nums[mid] == target){
+				if (mod == 0){ // get left bound
+					if(mid == 0 || nums[mid-1] < target)	
+						return mid;
+					else
+						high = mid;
+				}
+				if(mod == 1 ){ // get right bound
+					if(mid == nums.length-1 || nums[mid+1]>target)
+						return mid;
+					else
+						low = mid+1;
+				}
+			}
+			else if(nums[mid] < target)
+				low = mid+1;
+			else if(nums[mid] > target)
+				high = mid;
+		}
+		return nums[low] == target ? low : -1;
+	}
+	
+	//worst cast is o(n)
+	public int[] searchRange1(int[] nums, int target) {
 		int pos = helper(nums, target, 0, nums.length - 1);
 		if (pos == -1)
 			return new int[] { -1, -1 };
@@ -47,7 +83,7 @@ public class FirstandLastPosition {
 	public static void main(String[] args) {
 		FirstandLastPosition f = new FirstandLastPosition();
 		int[] a = {5, 7, 7, 8, 8, 10 };
-		//System.out.println(Arrays.toString(f.searchRange(a, 8)));
+		System.out.println(Arrays.toString(f.searchRange(a, 8)));
 		
 		int[] b = {1};
 		System.out.println(Arrays.toString(f.searchRange(b, 1)));
