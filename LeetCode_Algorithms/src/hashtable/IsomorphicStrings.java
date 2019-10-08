c64a3894434a011b85a7ecf62ab15118d22c7bd2
yibing jia <jiayibing1987@gmail.com>
2019-10-08 20:59:53 +1000

    #205

--- LeetCode_Algorithms/src/hashtable/IsomorphicStrings.java
+++ LeetCode_Algorithms/src/hashtable/IsomorphicStrings.java
@@ -13,6 +13,24 @@ import java.util.Map;
 public class IsomorphicStrings {
 
     public boolean isIsomorphic(String s, String t) {
+    	if(s == null || t == null) return false;
+    	if(s.length() != t.length()) return false;
+    	Map<Character, Integer> mapS = new HashMap<Character, Integer>();
+    	Map<Character, Integer> mapT = new HashMap<Character, Integer>();
+
+    	for(int i=0; i<s.length(); i++){
+    		int indexS = mapS.getOrDefault(s.charAt(i), -1);
+    		int indexT = mapT.getOrDefault(t.charAt(i), -1);
+    		if(indexS != indexT) return false;
+    		mapS.put(s.charAt(i), i);
+    		mapT.put(t.charAt(i), i);
+    	}
+    	return true;
+    }
+
+    public boolean isIsomorphic1(String s, String t) {
+    	if(s == null || t == null) return false;
+    	if(s.length() != t.length()) return false;
     	Map<Character, List<Integer>> mapS = this.getMap(s);
     	Map<Character, List<Integer>> mapT = this.getMap(t);
         for(Map.Entry<Character, List<Integer>> entry: mapS.entrySet()){
