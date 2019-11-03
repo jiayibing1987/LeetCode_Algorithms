cdd6d7942ce36c313d557b16ccc3b375eaadb191
yibing jia <jiayibing1987@gmail.com>
2019-11-03 14:52:18 +1000

    #219 improve layout of code

--- LeetCode_Algorithms/src/hashtable/ContainsDuplicatesTwo.java
+++ LeetCode_Algorithms/src/hashtable/ContainsDuplicatesTwo.java
@@ -16,14 +16,11 @@ public class ContainsDuplicatesTwo {
 	public boolean containsNearbyDuplicate(int[] nums, int k) {
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i < nums.length; i++) {
-			if (!map.containsKey(nums[i])) {
-				map.put(nums[i], i);
-			} else {
+			if (map.containsKey(nums[i])) {
 				if (Math.abs(i - map.get(nums[i])) <= k)
 					return true;
-				else
-					map.put(nums[i], i);
 			}
+			map.put(nums[i], i);
 		}
 		return false;
 	}
