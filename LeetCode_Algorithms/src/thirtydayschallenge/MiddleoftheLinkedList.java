49142030b22f785325438c132472f27efd591212
jiayibing1987 <jiayibing1987@gmail.com>
2020-04-10 15:16:45 +1000

    middle of linked list

---
+++ LeetCode_Algorithms/src/thirtydayschallenge/MiddleoftheLinkedList.java
@@ -0,0 +1,16 @@
+package thirtydayschallenge;
+
+import util.ListNode;
+
+public class MiddleoftheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
