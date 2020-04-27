7a47ea627b35e1a2ff54c11a66f0e5baee16ebc9
jiayibing1987 <jiayibing1987@gmail.com>
2020-04-27 22:44:29 +1000

    30 days challenge

---
+++ LeetCode_Algorithms/src/thirtydayschallenge/JumpGame.java
@@ -0,0 +1,16 @@
+package thirtydayschallenge;
+
+public class JumpGame {
+
+    public boolean canJump(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return false;
+        int max = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(max < i) return false;
+            max = Math.max(max, i+nums[i]);
+        }
+        return true;
+    }
+
+}
