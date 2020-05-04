64d72ca00bb8f2ddc779959e91e3e93f53e50e3a
jiayibing1987 <jiayibing1987@gmail.com>
2020-05-05 01:27:58 +1000

    30 days challenge

---
+++ LeetCode_Algorithms/src/thirtydayschallenge/NumberComplement.java
@@ -0,0 +1,15 @@
+package thirtydayschallenge;
+
+public class NumberComplement {
+
+    public int FindComplement(int num) {
+        int i = 0;
+        int j = 0;
+        while (i < num) {
+            i += Math.pow(2, j);
+            j++;
+        }
+        return i - num;
+    }
+
+}
