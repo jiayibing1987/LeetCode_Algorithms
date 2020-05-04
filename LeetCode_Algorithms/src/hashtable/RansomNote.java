9531a6949e3f786d2dbadaffac7d814cedbb431e
jiayibing1987 <jiayibing1987@gmail.com>
2020-05-04 16:37:11 +1000

    30 days challenge

---
+++ LeetCode_Algorithms/src/hashtable/RansomNote.java
@@ -0,0 +1,21 @@
+package hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RansomNote {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(Character c : magazine.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        for(Character c: ransomNote.toCharArray()) {
+            if(map.containsKey(c) && map.get(c) > 0)
+                map.put(c, map.get(c) - 1);
+            else
+                return false;
+        }
+        return true;
+    }
+
+}
