035e0d420aa9171de35ec6bc020dce182c104df0
jiayibing1987 <jiayibing1987@gmail.com>
2020-07-08 21:27:24 +1000

    30 days challenge

---
+++ LeetCode_Algorithms/src/thirtydayschallenge/ThreeSum.java
@@ -0,0 +1,38 @@
+package thirtydayschallenge;
+
+import java.util.*;
+
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> results = new HashSet<List<Integer>>();
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++) {
+             twoSum(nums, 0-nums[i], i, results);
+        }
+        return new ArrayList<>(results);
+    }
+
+    private void twoSum(int[] nums, int target, int index, Set<List<Integer>> results) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+            if(i==index) continue;
+            if(set.contains(nums[i])) continue;
+            if(map.containsKey(nums[i])){
+                List<Integer> temp = new ArrayList<>();
+                temp.add(nums[index]);
+                temp.add(nums[map.get(nums[i])]);
+                temp.add(nums[i]);
+                set.add(nums[i]);
+                set.add(nums[map.get(nums[i])]);
+                Collections.sort(temp);
+                results.add(temp);
+            }else{
+                map.put(target-nums[i], i);
+            }
+        }
+    }
+
+
+}
