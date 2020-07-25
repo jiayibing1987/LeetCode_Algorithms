9daffbecc255863635622bf0e9129e50b7638809
jiayibing1987 <jiayibing1987@gmail.com>
2020-07-25 13:06:23 +1000

    30 days challenge

--- LeetCode_Algorithms/src/thirtydayschallenge/BinaryTreeZigzagLevelOrderTraversal.java
+++ LeetCode_Algorithms/src/thirtydayschallenge/BinaryTreeZigzagLevelOrderTraversal.java
@@ -14,16 +14,17 @@ public class BinaryTreeZigzagLevelOrderTraversal {
         int depth = 0;
 
         while(!q.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
+            List<Integer> temp = new LinkedList<>();
             int size = q.size();
             for(int i=0; i<size; i++) {
                 TreeNode node = q.poll();
-                temp.add(node.val);
+                if(depth % 2 == 0)
+                    temp.add(node.val);
+                else
+                    temp.add(0, node.val);
                 if(node.left != null) q.add(node.left);
                 if(node.right != null) q.add(node.right);
             }
-            if(depth % 2 != 0)
-                Collections.reverse(temp);
             results.add(temp);
             depth++;
         }
