fb38a10664c602523deb88c11e5b2eac3a54389c
jiayibing1987 <jiayibing1987@gmail.com>
2020-12-20 11:38:16 +1000

    30 days challenge

--- LeetCode_Algorithms/src/array/LargestRectangleinHistogram.java
+++ LeetCode_Algorithms/src/array/LargestRectangleinHistogram.java
@@ -1,11 +1,56 @@
 package array;
 
+import java.util.Stack;
+
 /**
  * 84. Largest Rectangle in Histogram
  * @author Ryan
  *
  */
 public class LargestRectangleinHistogram {
+
+	//3. dp with stack
+	public int largestRectangleArea(int[] heights) {
+		if(heights == null || heights.length == 0)
+			return 0;
+		int n = heights.length;
+		int[] left = new int[n];
+		int[] right = new int[n];
+
+		Stack<Integer> stack = new Stack<>();
+		for(int i=0; i<n; i++) {
+			while(!stack.isEmpty() && heights[i] <= heights[stack.peek()]) {
+				stack.pop();
+			}
+			if(stack.isEmpty()) {
+				left[i] = -1;
+			} else {
+				left[i] = stack.peek();
+			}
+			stack.push(i);
+		}
+
+		stack.clear();
+		for(int i=n-1; i>=0; i--) {
+			while(!stack.isEmpty() && heights[i] <= heights[stack.peek()]) {
+				stack.pop();
+			}
+			if(stack.isEmpty()) {
+				right[i] = n;
+			} else {
+				right[i] = stack.peek();
+			}
+			stack.push(i);
+		}
+
+		int maxArea = 0;
+		for(int i=0; i<n; i++) {
+			int area = (right[i] - left[i] - 1) * heights[i];
+			maxArea = Math.max(maxArea, area);
+		}
+		return maxArea;
+	}
+
 
 	//1. brute force, O(n2)
     public int largestRectangleArea1(int[] heights) {
@@ -29,7 +74,7 @@ public class LargestRectangleinHistogram {
     //b) Maximum area in right side of minimum value (Not including the min value)
     //c) Number of bars multiplied by minimum value.
     //recusively find the max area for each part
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea2(int[] heights) {
     	return largestArea(heights, 0, heights.length-1);
     }
     
@@ -54,7 +99,9 @@ public class LargestRectangleinHistogram {
     
     
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LargestRectangleinHistogram l = new LargestRectangleinHistogram();
+		int[] a1 = new int[] {2,1,5,6,2,3};
+		System.out.println(l.largestRectangleArea(a1));
 	}
 
 }
