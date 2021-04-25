a6ca929bbef86a62dbd6cbe0a9d54789aab2db01
jiayibing1987 <jiayibing1987@gmail.com>
2021-04-25 17:09:56 +1000

    30 days challenge

---
+++ LeetCode_Algorithms/src/slidingwindow/LongestSubstringOfAllVowelsinOrder.java
@@ -0,0 +1,39 @@
+package slidingwindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringOfAllVowelsinOrder {
+
+    public int longestBeautifulSubstring(String word) {
+        Set<Character> set = new HashSet<>();
+        int n = word.length();
+        int max = 0;
+        int l = 0;
+        if (word.charAt(0) == 'a') set.add('a');
+        for (int r = 1; r < n; r++) {
+            if (word.charAt(r) == 'a' && word.charAt(r - 1) != 'a') {
+                l = r;
+                set.clear();
+                set.add('a');
+            } else {
+                if (word.charAt(r) >= word.charAt(r - 1)) {
+                    set.add(word.charAt(r));
+                    if (set.size() == 5) {
+                        max = Math.max(max, r - l + 1);
+                    }
+                } else {
+                    set.clear();
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstringOfAllVowelsinOrder l = new LongestSubstringOfAllVowelsinOrder();
+        System.out.println(l.longestBeautifulSubstring("aeiaaioaaaaeiiiiouuuooaauuaeiu"));
+        //System.out.println(f.maxFrequency(new int[]{1,4,8,13}, 5));
+        //System.out.println(f.maxFrequency(new int[]{10000}, 10000));
+    }
+}
