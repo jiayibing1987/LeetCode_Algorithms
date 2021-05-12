3ed89bb074dbae9fc4fb7c88454b971488917131
jiayibing1987 <jiayibing1987@gmail.com>
2021-05-12 18:24:43 +1000

    30 days challenge

---
+++ LeetCode_Algorithms/src/thirtydayschallenge/NumMatrix.java
@@ -0,0 +1,24 @@
+package thirtydayschallenge;
+
+public class NumMatrix {
+
+    int m;
+    int n;
+    int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        this.m = matrix.length;
+        this.n = matrix[0].length;
+        this.dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+}
