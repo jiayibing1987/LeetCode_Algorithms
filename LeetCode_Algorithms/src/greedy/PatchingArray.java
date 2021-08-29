ad051ca4aa049e77a6c9feb2a3ee5ec593836918
jiayibing1987 <jiayibing1987@gmail.com>
2021-08-29 22:28:51 +1000

    30 days challenge

--- LeetCode_Algorithms/src/greedy/PatchingArray.java
+++ LeetCode_Algorithms/src/greedy/PatchingArray.java
@@ -8,18 +8,12 @@ public class PatchingArray {
         int count = 0;
 
         while (coverd < n) {
-            if (i < nums.length) {
-                if (coverd  + 1 < nums[i]) {
-                    long miss = coverd + 1;
-                    count++;
-                    coverd = coverd + miss;
-                } else {
-                    coverd += nums[i++];
-                }
-            } else {
+            if (i >= nums.length || coverd + 1 < nums[i]) {
                 long miss = coverd + 1;
                 count++;
                 coverd = coverd + miss;
+            } else {
+                coverd += nums[i++];
             }
         }
         return count;
@@ -27,7 +21,7 @@ public class PatchingArray {
 
     public static void main(String[] args) {
         PatchingArray p = new PatchingArray();
-        int[] a1 = new int[] {1, 5, 10};
+        int[] a1 = new int[]{1, 5, 10};
         System.out.println(p.minPatches(a1, 20));
     }
 }
