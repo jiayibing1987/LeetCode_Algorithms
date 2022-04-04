165561c9a6f5d90676bc225ce46c54bd4c815b48
jiayibing1987 <jiayibing1987@gmail.com>
2022-04-04 21:12:50 +1000

    contest

--- LeetCode_Algorithms/src/util/ListNode.java
+++ LeetCode_Algorithms/src/util/ListNode.java
@@ -3,6 +3,7 @@ package util;
 public class ListNode {
 	  public int val;
 	  public ListNode next;
+	  public ListNode() {}
 	  public ListNode(int x) { val = x; }
 	  
 	  public String toString(){
