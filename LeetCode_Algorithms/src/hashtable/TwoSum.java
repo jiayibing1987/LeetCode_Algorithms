93f9c210b8970c705a7fac0350213b4f748b07d3
ryan.jia <ryan.jia@workday.com>
2025-03-11 13:16:21 +1000

    two sum

--- LeetCode_Algorithms/src/hashtable/TwoSum.java
+++ LeetCode_Algorithms/src/hashtable/TwoSum.java
@@ -11,24 +11,32 @@ import java.util.Map;
 public class TwoSum {
 	
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        
-        for(int i=0; i<nums.length; i++){
-        	if(map.containsKey(target - nums[i])){
-        		result[1] = i;
-        		result[0] = map.get(target - nums[i]);
-        		break;
-        	}      		
-        	map.put(nums[i], i);
-        }
-        return result;
+		int[] result = new int[2];
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i=0; i<nums.length; i++) {
+			map.put(nums[i], i);
+		}
+
+		for(int i=0; i<nums.length; i++) {
+			int lookFor = target - nums[i];
+			if(map.containsKey(lookFor) && map.get(lookFor) != i) {
+				result[0] = i;
+				result[1] = map.get(lookFor);
+				break;
+			}
+		}
+
+		return result;
     }
     
     public static void main (String[] args){
     	TwoSum ts = new TwoSum();
     	int[] nums = new int[]{2, 7, 11, 15};	
-    	System.out.println(ts.twoSum(nums, 9)[0]);
-    	System.out.println(ts.twoSum(nums, 9)[1]);
+    	//System.out.println(ts.twoSum(nums, 9)[0]);
+    	//System.out.println(ts.twoSum(nums, 9)[1]);
+
+		int[] nums1 = new int[]{3, 2, 4};
+		System.out.println(ts.twoSum(nums1, 6)[0]);
+		System.out.println(ts.twoSum(nums1, 6)[1]);
     }
 }
