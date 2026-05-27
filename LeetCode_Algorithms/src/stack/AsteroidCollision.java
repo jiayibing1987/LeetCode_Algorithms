2c6739884c794186093c95c97aa01831e5573314
ryanjia <jiayibing1987@gmail.com>
2026-05-27 17:05:59 +1000

    test

--- LeetCode_Algorithms/src/stack/AsteroidCollision.java
+++ LeetCode_Algorithms/src/stack/AsteroidCollision.java
@@ -31,9 +31,47 @@ public class AsteroidCollision {
         return res;
     }
 
+    public int[] asteroidCollision1(int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
+
+        for(int a : asteroids) {
+            while(!s.isEmpty() && s.peek() > 0 && a < 0) {
+                int b = s.peek();
+                if(a + b != 0) {
+                    if(Math.abs(a) > Math.abs(b)) {
+                        s.pop();
+                    } else {
+                        a = 0;
+                        break;
+                    }
+                } else {
+                    s.pop();
+                    a = 0;
+                    break;
+                }
+            }
+
+            if(a != 0)
+                s.push(a);
+        }
+
+        if(s.isEmpty()) return new int [0];
+        int size = s.size();
+        int[] res = new int [size];
+
+        for(int i = size - 1; i >= 0; i --) {
+            res[i] = s.pop();
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         AsteroidCollision a = new AsteroidCollision();
-        int[] array = new int[]{5,10,-5};
-        System.out.println(Arrays.toString(a.asteroidCollision(array)));
+        int[] array1 = new int[]{5,10,-5};
+        int[] array2= new int[]{8, -8};
+        System.out.println(Arrays.toString(a.asteroidCollision1(array1)));
+        System.out.println(Arrays.toString(a.asteroidCollision1(array2)));
+
     }
 }
